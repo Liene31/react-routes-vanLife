429f2aa8841f160e2c614da28b935a4264235d21
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-03-30 16:34:32 +0200

    add about page structure and styling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,29 @@
 export function About() {
-  return <h1>Hello from About</h1>;
+  return (
+    <main>
+      <section className="hero">
+        <img
+          src="../src/assets/hero-image.png"
+          alt="a person sitting on the van"
+        />
+      </section>
+      <section className="intro">
+        <h2>Don’t squeeze in a sedan when you could relax in a van.</h2>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </section>
+      <section className="cta">
+        <p>Your destination is waiting.</p>
+        <p>Your van is ready.</p>
+        <button>Explore our vans</button>
+      </section>
+    </main>
+  );
 }
