881f850f8da72658cba8b25ca3e11a679dadb4e1
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-03-31 19:56:20 +0200

    fetch van data from server

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,14 @@
+import React from "react";
+
 export function Vans() {
+  const [vans, setVans] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
   return (
     <main>
       <section className="van-search">
@@ -10,21 +20,23 @@ export function Vans() {
         </div>
       </section>
       <section className="van-cards">
-        <div className="van-card">
-          <img
-            src="../src/assets/hero-image.png"
-            alt="vans"
-            className="van-img"
-          />
-          <div className="van-info">
-            <h2>Modest Explorer</h2>
-            <div className="price">
-              <p className="amount">$60</p>
-              <p className="unit">/day</p>
+        {vans.map((van) => {
+          return (
+            <div key={van.id} className="van-card">
+              <img src={van.imageUrl} alt={van.name} className="van-img" />
+              <div className="van-info">
+                <h2>{van.name}</h2>
+                <div className="price">
+                  <p className="amount">${van.price}</p>
+                  <p className="unit">/day</p>
+                </div>
+              </div>
+              <button className={`btn van-type-btn ${van.type}`}>
+                {van.type}
+              </button>
             </div>
-          </div>
-          <button className="btn van-type-btn">Simple</button>
-        </div>
+          );
+        })}
       </section>
     </main>
   );
