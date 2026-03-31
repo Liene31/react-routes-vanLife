5e6add28629b3279a50aa42d7acc721f57dce6bc
Liene <lienebare@gmail.com>
2026-03-31 09:46:05 +0200

    add van structure

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,3 +1,31 @@
 export function Vans() {
-  return <h1>hello from Vans</h1>;
+  return (
+    <main>
+      <section className="van-search">
+        <h1>Explore our van options</h1>
+        <div className="van-search-options">
+          <button className="btn van-search-option">Simple</button>
+          <button className="btn van-search-option">Luxury</button>
+          <button className="btn van-search-option">Rugged</button>
+        </div>
+      </section>
+      <section className="van-cards">
+        <div className="van-card">
+          <img
+            src="../src/assets/hero-image.png"
+            alt="vans"
+            className="van-img"
+          />
+          <div className="van-info">
+            <h2>Modest Explorer</h2>
+            <div className="price">
+              <p className="amount">$60</p>
+              <p className="unit">/day</p>
+            </div>
+          </div>
+          <button className="btn van-type-btn">Simple</button>
+        </div>
+      </section>
+    </main>
+  );
 }
