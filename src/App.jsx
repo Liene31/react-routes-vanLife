29cef61c3e40a58e1fcdb33c73c6dd6e1b416905
Liene <lienebare@gmail.com>
2026-04-02 09:25:02 +0200

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,18 @@ import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
 import { Vans } from "./pages/Vans.jsx";
 import { VanDetail } from "./pages/VanDetail.jsx";
+import { Layout } from "./components/Layout.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="nav">
-        <Link className="logo" to="/">
-          #VANLIFE
-        </Link>
-        <div className="nav-links">
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </div>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
