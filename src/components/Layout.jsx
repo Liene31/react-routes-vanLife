af245c30b2d761278a424f7dd0fe8b7c3d0bfb53
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-04-02 20:52:23 +0200

    add footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header.jsx";
+import { Footer } from "./Footer.jsx";
 
 export function Layout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
